cc5eae9530d9d645a18aa3b9701baec6b353974a
Henrik Andersen <han@rts.dk>
2026-03-11 22:00:04 +0100

    feat: update routing to use dedicated page components for Home, About, Cast, and Archive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import CastPage from "./pages/CastPage";
+import ArchivePage from "./pages/ArchivePage";
 
 function App() {
 
   const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <HomePage />,
   },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/cast",
+    element: <CastPage />,
+  },
+  {
+    path: "/archive",
+    element: <ArchivePage />,
+  }
 ]);
 
   return (
