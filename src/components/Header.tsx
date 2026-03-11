133b7b0653162a15977747c0dc9901452af483d9
Henrik Andersen <han@rts.dk>
2026-03-11 22:00:18 +0100

    feat: add Header component with navigation and logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Logo from "../assets/ui/fertile-lands-logo.png";
+
+export default function Header() {
+
+    return (
+        <header>
+            <a href="/">
+                <img src={Logo} alt="Fertile Lands" />
+            </a>
+            <nav className="bg-accent border-y border-secondary ">
+                <ul className="flex gap-4 p-2 justify-end">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/cast">Cast</a></li>
+                    <li><a href="/archive">Archive</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
