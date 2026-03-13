933fd2c42f72bd94a53fc2c09f26d209309701b5
Henrik Andersen <han@rts.dk>
2026-03-13 23:17:24 +0100

    feat: add Netlify integration and new navigation assets
    - Added Netlify development command to package.json.
    - Included new font "@fontsource/architects-daughter" in dependencies and applied it to the body.
    - Updated PageNavigation component to use new navigation assets.
    - Enhanced Header component with hover effects.
    - Improved ComicPage with additional styling for the description.
    - Fixed minor syntax issue in App component.
    - Added new navigation images for last and next pages.
    - Updated Vite configuration to include Netlify plugin.

--- src/components/PageNavigation.tsx
+++ src/components/PageNavigation.tsx
@@ -1,16 +1,14 @@
-import PreviousArrow from '../assets/ui/navigation/nav_previous.png';
-import NextArrow from '../assets/ui/navigation/nav_next.png';
-import FirstArrow from '../assets/ui/navigation/nav_first.png';
-import LastArrow from '../assets/ui/navigation/nav_last.png';
+import NextArrow from '../assets/ui/navigation/nav_next2.png';
+import LastArrow from '../assets/ui/navigation/nav_last2.png';
 import { Link } from 'react-router';
 
 export default function PageNavigation({ currentPage }: { currentPage: number }) {
 
     return (
         <nav className="flex gap-4 sticky bottom-2">
-            <ul className='flex mx-auto p-4 max-w-3xs'>
-                <li><Link to="/comic/first"><img src={FirstArrow} alt="First page" /></Link></li>
-                <li><Link to={`/comic/${currentPage - 1}`}><img src={PreviousArrow} alt="Previous page" /></Link></li>
+            <ul className='flex mx-auto p-4 max-w-md gap-1 [&>li]:hover:scale-105 [&>li]:hover:transition-transform'>
+                <li><Link to="/comic/first"><img src={LastArrow} alt="First page" className='scale-x-[-1]' /></Link></li>
+                <li><Link to={`/comic/${currentPage - 1}`}><img src={NextArrow} alt="Previous page" className='scale-x-[-1]' /></Link></li>
                 <li><Link to={`/comic/${currentPage + 1}`}><img src={NextArrow} alt="Next page" /></Link></li>
                 <li><Link to="/comic/latest"><img src={LastArrow} alt="Latest page" /></Link></li>
             </ul>
--- src/pages/ComicPage.tsx
+++ src/pages/ComicPage.tsx
@@ -27,7 +27,7 @@ export default function ComicPage() {
                 <img className="bg-white aspect-9/12 max-w-2xl w-full flex justify-center items-center mx-auto m-4 drop-shadow-md" src={`/pages/dummies/${pageId}.png`} alt={`Comic ${pageId}`} />
                 <div className="bg-accent border-y border-secondary min-h-10" />
 
-                <p className="bg-white m-4 max-w-2xl mx-auto p-4">Blah blah blah comic stuff</p>
+                <p className="bg-white m-4 max-w-2xl mx-auto p-4 shadow-md">Blah blah blah comic stuff</p>
             </main>
             <PageNavigation currentPage={Number(pageId)} />
 
